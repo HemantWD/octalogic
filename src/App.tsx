4d649199c83076e813101f834ae588505a2d0714
HemantWD <Hemantajaysharma@gmail.com>
2023-09-11 19:50:49 +0530

    integrated shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="grid place-items-center h-screen">
+        <Button variant="destructive">Click Me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
